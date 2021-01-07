ac488e6cd86a0a6868765f519e37211b25bdb7e6
Vinicius Freitas de Almeida <vinifreitas.d.a@gmail.com>
2021-01-07 19:00:23 -0300

    Now parsing command-line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
-use std::panic;
+use std::{env, panic};
 
 use scicalc_rs::parser::eval;
+
+fn show_usage() {
+    println!("Usage: scicalc-rs [expression]");
+}
 fn main() {
     panic::set_hook(Box::new(|_info| {
         // do nothing
         //TODO: Add proper error handling
     }));
-    let result = panic::catch_unwind(|| {
-        eval("(1.0 ± 0.1) * (3.0 ± 0.1)")
-    });
-    match result {
-        Ok(res) => println!("{}", res),
-        Err(_) => println!("ERROR"),
+
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() <= 1 {
+        println!("Error: Not enough args.");
+        show_usage();
+    } else if args.len() == 2 {
+        let result = panic::catch_unwind(|| {
+            let text = args[1].as_str();
+            eval(text)
+        });
+        match result {
+            Ok(res) => println!("{}", res),
+            Err(_) => println!("Error: could not parse expression."),
+        }
+    } else {
+        println!("Error: too many args.");
+        show_usage();
     }
 }
