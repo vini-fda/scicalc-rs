75dcfba70bb1045ea3c193e1d2bf961d219bc9dd
Vinicius Freitas de Almeida <vinifreitas.d.a@gmail.com>
2021-01-07 16:14:13 -0300

    Added fmt::Display trait, removed Msmnt and add +-

--- src/token.rs
+++ src/token.rs
@@ -1,14 +1,16 @@
 use crate::decimal::DecimalNumber;
-use crate::measurement::Measurement;
+use std::fmt;
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Token {
     PosNum(DecimalNumber), //Positive number literal
-    Msmnt(Measurement),    //Measurement literal
 
     //Constants
     EulersNum, //Euler's number
     Pi,        //pi
+
+    //Measurement
+    PlusMinus, //'±', used as a separator in measurements
 
     //Operators
     Add,   //'+'
@@ -22,4 +24,23 @@ pub enum Token {
 
     //End-of-file
     Eof,
+}
+
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Token::PosNum(d) => write!(f, "{}", d),
+            Token::EulersNum => write!(f, "e"),
+            Token::Pi => write!(f, "π"),
+            Token::PlusMinus => write!(f, "±"),
+            Token::Add => write!(f, "+"),
+            Token::Minus => write!(f, "-"),
+            Token::Mul => write!(f, "*"),
+            Token::Div => write!(f, "/"),
+            Token::LeftParen => write!(f, "("),
+            Token::RightParen => write!(f, ")"),
+            Token::Eof => write!(f, "EOF"), //useful for debugging
+        }
+    }
 }
